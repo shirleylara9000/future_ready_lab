23b7b410417f21fd9f0bbb16068ffc0995b78df1
shirleylara9000 <shirleylara9000@yahoo.com>
2018-06-26 12:00:52 -0700

    I added data validation.

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -5,17 +5,33 @@ alert("First enter a low number, then a high number. Then, guess a random betwee
 //get the low and high bounds
 //uses parseInt() to make sure we have numbers not strings
 var from = parseInt(prompt("Enter the lower bound."));
-
-var to = parseInt(prompt("Enter the higher bound."));
-
+while(from < 0 || from > 1000){
+   from = parseInt(prompt("Enter a number greater than 0"));
+}
+while (isNaN(from)){
+   from = parseInt(prompt("You must enter a number."));
+}
+// must enter a number not letter     
+var to = parseInt(prompt("Enter the higher bound."));   
+while (isNaN(to)){
+  to = parseInt(prompt("You must enter a number."));
+} 
+// higher bound must be higher than lower bound
+while(to <= from){
+    to = parseInt(prompt("Your higher bound must be greater then your lower bound."));
+}
 //get an integer between [from,to]
 //Math.random() returns decimals, used Math.round() to get whole number
 var target = Math.round(Math.random() * (to - from) + from);
 
 var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
-
+// user may only enter guess between from and to.
 var totalGuesses = 1;
 
+// guesses must be only numbers
+while (isNaN(currentGuess)){
+    currentGuess = parseInt(prompt("You must enter a number."));
+}
 // loop until user guesses correct number
 while(currentGuess != target){
      if (currentGuess < target){
@@ -26,9 +42,10 @@ while(currentGuess != target){
     currentGuess = parseInt(prompt("Enter a lower number"));
     
     totalGuesses++; 
+}else if (currentGuess < from || currentGuess > to){
+    currentGuess = parseInt(prompt("You can only guess between your lower and higher bound."))
 }
 }
 
 alert("It took " + totalGuesses + " tries to guess the correct number.");
 }
-
